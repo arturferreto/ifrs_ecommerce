ec069b397b906fd0fb20c81f405638973b2b15fe
arturferreto <arturpessuto@gmail.com>
2024-06-18 23:03:03 -0300

    feat: adicionando rota para buscar usuários e perfil

---
+++ src/main/java/com/ifrs/ecommerce/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.ifrs.ecommerce.controllers;
+
+import com.ifrs.ecommerce.models.User;
+import com.ifrs.ecommerce.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        List <User> users = userService.allUsers();
+
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+}
