3c07b6d227c806e0c309970b2b60d43896dca009
arturferreto <arturpessuto@gmail.com>
2024-07-02 20:34:40 -0300

    refactor: removendo visualização de todos usuários

--- src/main/java/com/ifrs/ecommerce/services/UserService.java
+++ src/main/java/com/ifrs/ecommerce/services/UserService.java
@@ -1,11 +1,7 @@
 package com.ifrs.ecommerce.services;
 
-import com.ifrs.ecommerce.models.User;
 import com.ifrs.ecommerce.repositories.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
@@ -13,13 +9,5 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    public List<User> allUsers() {
-        List<User> users = new ArrayList<>();
-
-        userRepository.findAll().forEach(users::add);
-
-        return users;
     }
 }
