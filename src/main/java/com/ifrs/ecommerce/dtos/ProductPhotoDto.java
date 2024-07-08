cb5784a4ee0b99d7c6257122a7866a382c49746b
arturferreto <arturpessuto@gmail.com>
2024-07-08 01:43:50 -0300

    fix: ajustes no cadastro de imagens de produtos

--- src/main/java/com/ifrs/ecommerce/dtos/ProductPhotoDto.java
+++ src/main/java/com/ifrs/ecommerce/dtos/ProductPhotoDto.java
@@ -1,10 +1,8 @@
 package com.ifrs.ecommerce.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record ProductPhotoDto(
-    @NotNull(message = "O id do produto não foi encontrado.") Integer productId,
     @NotBlank(message = "A URL não foi informada.") String photoUrl,
     boolean isFavorite
 ) {
