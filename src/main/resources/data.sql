c4703dbc3c1eee5d5edd713c136fb99054d4f1e9
arturferreto <arturpessuto@gmail.com>
2024-07-09 23:41:16 -0300

    feat: criando insert de dados no banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,36 @@
+insert into discounts (id, code, quantity, percentage) values (1, 'PRIMEIRACOMPRA', 1, 10);
+
+insert into products (id, name, description, price, favorite_photo_url) values (1, 'MULTI PANEL UTILITY JACKET', 'WASHED COTTON, SHOULDER POCKETS, VELCRO CLOSURES, ANTHRACITE RIBBON, RED FLAG LABEL', 260, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW003FA03_BEG0016_1_KHAKI_16a4b51c-0388-40de-8697-107847f4b0e7.jpg?v=1712081135&width=1400&height=1400&crop=center');
+insert into product_features (id, product_id, quantity, name) values (1, 1, 0, 'S');
+insert into product_features (id, product_id, quantity, name) values (2, 1, 0, 'M');
+insert into product_features (id, product_id, quantity, name) values (3, 1, 1, 'L');
+insert into product_features (id, product_id, quantity, name) values (4, 1, 3, 'XL');
+insert into product_photos (id, product_id, photo_url) values (1, 1, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW003FA03_BEG0016_1_KHAKI_16a4b51c-0388-40de-8697-107847f4b0e7.jpg?v=1712081135&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (2, 1, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW003FA03_BEG0016_2_KHAKI.jpg?v=1712081134&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (3, 1, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW003FA03_BEG0016_4_KHAKI.jpg?v=1712081134&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (4, 1, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW003FA03_BEG0016_3_KHAKI.jpg?v=1712081135&width=1400&height=1400&crop=center');
+
+insert into products (id, name, description, price, favorite_photo_url) values (2, 'LET CHAOS REIGN LONG SLEEVE', 'MADE IN THE USA, SCREEN PRINTED IN THE USA, 100% COTTON, CUT & SEW CUSTOM BODY', 66, 'https://cdn.shopify.com/s/files/1/2068/4441/files/METAL_LS_TEE.jpg?v=1712246055&width=1400&height=1400&crop=center');
+insert into product_features (id, product_id, quantity, name) values (5, 2, 0, 'S');
+insert into product_features (id, product_id, quantity, name) values (6, 2, 1, 'M');
+insert into product_features (id, product_id, quantity, name) values (7, 2, 3, 'L');
+insert into product_features (id, product_id, quantity, name) values (8, 2, 2, 'XL');
+insert into product_photos (id, product_id, photo_url) values (5, 2, 'https://cdn.shopify.com/s/files/1/2068/4441/files/METAL_LS_TEE.jpg?v=1712246055&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (6, 2, 'https://cdn.shopify.com/s/files/1/2068/4441/files/METAL_LS_BACK.jpg?v=1712246055&width=1400&height=1400&crop=center');
+
+insert into products (id, name, description, price, favorite_photo_url) values (3, 'CAMP COLLAR SHIRT', 'MADE IN ITALY, ALL OVER PHOTO PRINT, FLUID VISCOSE', 160, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW007FA06_MLT0001_1_BROWN_c0f90a76-2400-4b99-bc76-007cc2745729.jpg?v=1709589222&width=1400&height=1400&crop=center');
+insert into product_features (id, product_id, quantity, name) values (9, 3, 4, 'S');
+insert into product_features (id, product_id, quantity, name) values (10, 3, 2, 'M');
+insert into product_features (id, product_id, quantity, name) values (11, 3, 1, 'L');
+insert into product_features (id, product_id, quantity, name) values (12, 3, 0, 'XL');
+insert into product_photos (id, product_id, photo_url) values (7, 3, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW007FA06_MLT0001_1_BROWN_c0f90a76-2400-4b99-bc76-007cc2745729.jpg?v=1709589222&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (8, 3, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW007FA06_MLT0001_2_BROWN.jpg?v=1709589222&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (9, 3, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBMW007FA06_MLT0001_3_BROWN.jpg?v=1709589222&width=1400&height=1400&crop=center');
+
+insert into products (id, name, description, price, favorite_photo_url) values (4, 'CERAMIC PIGGY BANK', 'CERAMIC PIGGY BANK, LENGTH 8"; WIDTH 6"; HEIGHT 5"', 60, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_2.jpg?v=1709767144&width=1400&height=1400&crop=center');
+insert into product_features (id, product_id, quantity, name) values (13, 4, 5, 'ONE SIZE');
+insert into product_photos (id, product_id, photo_url) values (10, 4, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_2.jpg?v=1709767144&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (11, 4, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_3.jpg?v=1709767144&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (12, 4, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_1.jpg?v=1709767144&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (13, 4, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_5.jpg?v=1709767144&width=1400&height=1400&crop=center');
+insert into product_photos (id, product_id, photo_url) values (14, 4, 'https://cdn.shopify.com/s/files/1/2068/4441/files/TBUW301OT01_PNK0006_4.jpg?v=1709767145&width=1400&height=1400&crop=center');
